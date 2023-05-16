c230d08540232024ef5ec06298745466cab157eb
2023JoshuaP <131817672+2023JoshuaP@users.noreply.github.com>
2023-05-16 18:46:48 -0500

    Add files via upload

--- DynamicArrayTemplate.cpp
+++ DynamicArrayTemplate.cpp
@@ -24,9 +24,9 @@ template <typename T> DynamicArray<T>::DynamicArray(){
 
 template <typename T> DynamicArray<T>::DynamicArray(T arr[], int size){
     this->size=size;
-    data= new T[size];
-    for (int i=0;i<size;i++)
-        data[i]=arr[i];
+    data = new T[size];
+    for (int i = 0;i < size; i++)
+        data[i] = arr[i];
 }
 
 template <typename T> DynamicArray<T>::DynamicArray(const DynamicArray &o){
@@ -87,6 +87,20 @@ template <typename T>DynamicArray<T>::~DynamicArray(){
     delete [] data;
 }
 
+class Estudiante{
+	private:
+		string nombre;
+	public:
+		Estudiante(){};
+    	Estudiante(const string& nombre) : nombre(nombre) {}
+    	friend ostream& operator<<(ostream& os, const Estudiante& estudiante);
+};
+
+ostream& operator<<(ostream& os, const Estudiante& estudiante) {
+    os << estudiante.nombre;
+    return os;
+}
+
 int main(){
 	int opc;
 	cout << "Elije: ";
@@ -106,7 +120,7 @@ int main(){
     	d1.remove(4);
     	d1.print();
 	}
-	if(opc == 2){
+	else if(opc == 2){
 		float arr2[3]={1.3,2.2,3.9};
     	DynamicArray<float> d2(arr2,3);
     	cout << "Array Original" << endl;
@@ -115,13 +129,13 @@ int main(){
     	d2.pushback(4.1);
     	d2.print();
     	cout << "Insert" << endl;
-    	d2.insert(0,1.4);
+    	d2.insert(9.8,1);
     	d2.print();
     	cout << "Remove" << endl;
     	d2.remove(4.1);
     	d2.print();
 	}
-	if(opc == 3){
+	else if(opc == 3){
 		char arr3[3]={'h','o','m'};
     	DynamicArray<char> d3(arr3,3);
     	cout << "Array Original" << endl;
@@ -136,7 +150,7 @@ int main(){
     	d3.remove('a');
     	d3.print();
 	}
-	if(opc == 4){
+	else if(opc == 4){
 		double arr4[3]={1.4,2,3.1};
     	DynamicArray<double> d4(arr4,3);
     	cout << "Array Original" << endl;
@@ -146,6 +160,22 @@ int main(){
     	d4.print();
     	cout << "Insert" << endl;
     	d4.insert(1.7,1);
+    	d4.print();
+    	cout << "Remove" << endl;
+    	d4.remove(2);
+    	d4.print();
+	}
+	else if(opc == 5){
+		Estudiante e1("A"), e2("B"), e3("C"), e4(""), e5("");
+		Estudiante arr5[3] = {e1,e2,e3};
+    	DynamicArray<Estudiante> d4(arr5,3);
+    	cout << "Array Original" << endl;
+    	d4.print();
+    	cout << "Pushback" << endl;
+    	d4.pushback(e4);
+    	d4.print();
+    	cout << "Insert" << endl;
+    	d4.insert(e5,1);
     	d4.print();
     	cout << "Remove" << endl;
     	d4.remove(2);
